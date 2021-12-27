d82638e628f4890ee2bce14717573e7aff5a7052
23rd <23rd@vivaldi.net>
2021-12-28 02:16:22 +0300

    Added option to hide "Send As" button.

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -1880,6 +1880,9 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
 
         @Override
         public TLRPC.TL_channels_sendAsPeers getSendAsPeers() {
+            if (MessagesController.getGlobalMainSettings().getBoolean("hideSendAs", false)) {
+                return null;
+            }
             return sendAsPeersObj;
         }
 
