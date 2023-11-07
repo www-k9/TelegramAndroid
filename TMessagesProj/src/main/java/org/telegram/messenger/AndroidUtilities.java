464249faee5065b616f6682160a18b6de51c1588
23rd <23rd@vivaldi.net>
2023-11-07 14:26:57 +0300

    Added option to send large photos.

--- TMessagesProj/src/main/java/org/telegram/messenger/AndroidUtilities.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/AndroidUtilities.java
@@ -2506,7 +2506,7 @@ public class AndroidUtilities {
 
     public static int getPhotoSize() {
         if (photoSize == null) {
-            photoSize = 1280;
+            photoSize = 1280 * SharedConfig.photoSizeFactor;
         }
         return photoSize;
     }
